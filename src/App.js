9cccf403aa5b657db8ece52a0e22d3fc372923dc
Irene <terna.legend@gmail.com>
2023-04-06 18:00:36 +0300

    added root properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      Hello I`m Oleksandra!
+    </div>
+  );
 }
 
 export default App;
