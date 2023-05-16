7bbeea17f193715953228b74676b278de68ff83c
Irene <terna.legend@gmail.com>
2023-05-16 17:07:11 +0200

    Added Home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+      <div className='home__social'>
+          <a href="#" className="home__social-icon" target="_blank"><i class="uil uil-instagram-alt"></i></a>
+
+          <a href="#" className="home__social-icon" target="_blank"><i class="uil uil-telegram-alt"></i></a>
+
+          <a href="#" className="home__social-icon" target="_blank"><i class="fa-brands fa-tiktok"></i></a>
+    </div>
+  )
+}
+
+export default Social
