0a5e2290773899392afa4f6c7b89df25d8664fbf
Irene <terna.legend@gmail.com>
2023-08-18 19:01:10 +0300

    Added contact form and testimonials slider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,8 @@ function App() {
         <Home />
         <About />
         <Qualification />
+        <Testimonials />
+        <Contact />
       </main>
     </div>
   );
