6961cb1c2a800b5fdc5ac9a97d509dbdfcdc88bc
Irene <terna.legend@gmail.com>
2023-08-19 14:35:16 +0300

    Added scroll to top

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 function App() {
   return (
     <div className="App">
@@ -16,8 +17,9 @@ function App() {
         <Qualification />
         <Testimonials />
         <Contact />
-        <Footer />
       </main>
+      <Footer />
+      <ScrollUp />
     </div>
   );
 }
