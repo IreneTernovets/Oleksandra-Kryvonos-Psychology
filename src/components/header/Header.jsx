75ee6ffba82e5551fcb954d8f1792341f8425072
Irene <terna.legend@gmail.com>
2023-08-20 17:12:13 +0300

    Added highlight of selected link in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,56 +3,70 @@ import { useState } from 'react';
 import "./header.css"
 
 const Header = () => {
+
+    /*=============== Change background header ===============*/
+
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header");
+        if (this.scrollY >= 80) {
+            header.classList.add("scroll-header");
+        }
+        else {
+            header.classList.remove("scroll-header")
+        }
+    })
+
     /*=============== Toggle Menu ===============*/
     const [Toggle, showMenu] = useState(false);
-  return (
-      <header className="header">
-          <nav className="nav container">
-              <a href="index.html" className="nav__logo">
-                  Kryvonos
-              </a>
-              <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-                  <ul className="nav__list grid">
-                      <li className="nav__item">
-                          <a href="#home" className="nav__link active-link">
-                              <i className="uil uil-estate nav__icon"></i>Головна
-                          </a>
-                      </li>
+    const [activeNav, setActiveNav] = useState("#home")
+    return (
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">
+                    Kryvonos
+                </a>
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
+                                <i className="uil uil-estate nav__icon"></i>Головна
+                            </a>
+                        </li>
 
-                      <li className="nav__item">
-                          <a href="#about" className="nav__link">
-                              <i className="uil uil-user nav__icon"></i> Про мене
-                          </a>
-                      </li>
+                        <li className="nav__item">
+                            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? "nav__link active-link" : "nav__link"}>
+                                <i className="uil uil-user nav__icon"></i> Про мене
+                            </a>
+                        </li>
 
-                      <li className="nav__item">
-                          <a href="#education" className="nav__link">
-                              <i className="uil uil-briefcase nav__icon"></i> Освіта
-                          </a>
-                      </li>
+                        <li className="nav__item">
+                            <a href="#education" onClick={() => setActiveNav('#education')} className={activeNav === "#education" ? "nav__link active-link" : "nav__link"}>
+                                <i className="uil uil-briefcase nav__icon"></i> Освіта
+                            </a>
+                        </li>
 
-                      <li className="nav__item">
-                          <a href="#services" className="nav__link">
-                              <i className="uil uil-clipboard-notes nav__icon"></i> Послуги
-                          </a>
-                      </li>
+                        <li className="nav__item">
+                            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === "#services" ? "nav__link active-link" : "nav__link"}>
+                                <i className="uil uil-clipboard-notes nav__icon"></i> Послуги
+                            </a>
+                        </li>
 
-                      <li className="nav__item">
-                          <a href="#contact" className="nav__link">
-                              <i className="uil uil-message nav__icon"></i> Контакти
-                          </a>
-                      </li>
-                  </ul>
+                        <li className="nav__item">
+                            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"} >
+                                <i className="uil uil-message nav__icon"></i> Контакти
+                            </a>
+                        </li>
+                    </ul>
 
-                  <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
-              </div>
+                    <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+                </div>
 
-              <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-                  <i class="uil uil-apps"></i>
-              </div>
-          </nav>
-    </header>
-  )
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <i class="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -6,157 +6,157 @@ const Qualification = () => {
     const toggleTab = (index) => {
         setToggleState(index);
     }
-  return (
-    <section className='qualification section'>
-          <h2 className="section__title">Кваліфікація</h2>
-          <span className="section__subtitle">Мій професійний шлях</span>
+    return (
+        <section className='qualification section' id='education'>
+            <h2 className="section__title">Кваліфікація</h2>
+            <span className="section__subtitle">Мій професійний шлях</span>
 
-          <div className="qualification__container container">
-              <div className="qualification__tabs">
-                  <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
-                  onClick={() => toggleTab(1)}>
-                      <i className="uil uil-graduation-cap qualification__icon"></i> Освіта
-                  </div>
+            <div className="qualification__container container">
+                <div className="qualification__tabs">
+                    <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
+                        onClick={() => toggleTab(1)}>
+                        <i className="uil uil-graduation-cap qualification__icon"></i> Освіта
+                    </div>
 
-                  <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
-                  onClick={() => toggleTab(2)}>
-                      <i className="uil uil-briefcase-alt qualification__icon"></i> Досвід
-                  </div>
-              </div>
+                    <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
+                        onClick={() => toggleTab(2)}>
+                        <i className="uil uil-briefcase-alt qualification__icon"></i> Досвід
+                    </div>
+                </div>
 
-              <div className="qualification__sections">
-                  <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
-                      <div className="qualification__data">
-                          <div>
-                              <h3 className="gualification__title">
-                                  Психологія
-                              </h3>
-                              <span className='qualification__subtitle'> Університет Драгоманова</span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2021 - нині
-                              </div>
-                          </div>
+                <div className="qualification__sections">
+                    <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="gualification__title">
+                                    Психологія
+                                </h3>
+                                <span className='qualification__subtitle'> Університет Драгоманова</span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - нині
+                                </div>
+                            </div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
-                      </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
 
-                      <div className="qualification__data">
-                          <div></div>
+                        <div className="qualification__data">
+                            <div></div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
 
-                          <div>
-                              <h3 className="gualification__title">
-                                  Психологія
-                              </h3>
-                              <span className='qualification__subtitle'> Університет Драгоманова</span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2021 - нині
-                              </div>
-                          </div>
-                      </div>
+                            <div>
+                                <h3 className="gualification__title">
+                                    Психологія
+                                </h3>
+                                <span className='qualification__subtitle'> Університет Драгоманова</span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - нині
+                                </div>
+                            </div>
+                        </div>
 
-                      <div className="qualification__data">
-                          <div>
-                              <h3 className="gualification__title">
-                                  Психологія
-                              </h3>
-                              <span className='qualification__subtitle'> Your Norma </span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2022 - нині
-                              </div>
-                          </div>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="gualification__title">
+                                    Психологія
+                                </h3>
+                                <span className='qualification__subtitle'> Your Norma </span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2022 - нині
+                                </div>
+                            </div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
-                      </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
 
-                      <div className="qualification__data">
-                          <div></div>
+                        <div className="qualification__data">
+                            <div></div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
 
-                          <div>
-                              <h3 className="gualification__title">
-                                  Психологія
-                              </h3>
-                              <span className='qualification__subtitle'> Psy простір</span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2021 - 2023
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
-                      <div className="qualification__data">
-                          <div>
-                              <h3 className="gualification__title">
-                                  Консультації
-                              </h3>
-                              <span className='qualification__subtitle'> Your Norma</span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2021 - 2023
-                              </div>
-                          </div>
+                            <div>
+                                <h3 className="gualification__title">
+                                    Психологія
+                                </h3>
+                                <span className='qualification__subtitle'> Psy простір</span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2023
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="gualification__title">
+                                    Консультації
+                                </h3>
+                                <span className='qualification__subtitle'> Your Norma</span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2023
+                                </div>
+                            </div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
-                      </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
 
-                      <div className="qualification__data">
-                          <div></div>
+                        <div className="qualification__data">
+                            <div></div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
 
-                          <div>
-                              <h3 className="gualification__title">
-                                  Консультації
-                              </h3>
-                              <span className='qualification__subtitle'> Університет Драгоманова - Київ</span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2021 - нині
-                              </div>
-                          </div>
-                      </div>
+                            <div>
+                                <h3 className="gualification__title">
+                                    Консультації
+                                </h3>
+                                <span className='qualification__subtitle'> Університет Драгоманова - Київ</span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - нині
+                                </div>
+                            </div>
+                        </div>
 
-                      <div className="qualification__data">
-                          <div>
-                              <h3 className="gualification__title">
-                                  Консультації
-                              </h3>
-                              <span className='qualification__subtitle'> Your Norma </span>
-                              <div className="qualififcation__calender">
-                                  <i className="uil uil-calendar-alt"></i> 2022 - нині
-                              </div>
-                          </div>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="gualification__title">
+                                    Консультації
+                                </h3>
+                                <span className='qualification__subtitle'> Your Norma </span>
+                                <div className="qualififcation__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2022 - нині
+                                </div>
+                            </div>
 
-                          <div>
-                              <span className="qualification__rounder"></span>
-                              <span className="qualification__line"></span>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-        
-    </section>
-  )
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
 }
 
 export default Qualification
